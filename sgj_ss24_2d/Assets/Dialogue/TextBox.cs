0bb5e206c799f3ac59ce6126476434c9ce5495bb
gringames <grin.games@proton.me>
2024-06-08 11:45:49 +0200

    add buttons to show text box

--- sgj_ss24_2d/Assets/Dialogue/TextBox.cs
+++ sgj_ss24_2d/Assets/Dialogue/TextBox.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,19 +8,22 @@ namespace Dialogue
 {
     public class TextBox : MonoBehaviour
     {
-        [Header("Points")]
-        [SerializeField] private Transform belowScreenPoint;
-        [SerializeField] private Transform inScreenPoint;
+        [Header("Points")] [SerializeField] private Transform belowScreenPoint;
 
         [Header("Animation Properties")] [SerializeField]
-        private float duration;
-        
+        private float duration = 1;
+
         // private properties
         private DOTweenAnimation _doTweenAnimation;
+        private TMP_Text _textComponent;
+
+
+        #region Init
 
         private void Awake()
         {
             transform.position = belowScreenPoint.position;
+            _textComponent = GetComponentInChildren<TMP_Text>();
             InitAnimation();
         }
 
@@ -28,16 +32,43 @@ namespace Dialogue
             _doTweenAnimation = GetComponent<DOTweenAnimation>();
             _doTweenAnimation.duration = duration;
         }
+
+        #endregion
+
+        public void DisplayTextBox(string text)
+        {
+            SetText(text);
+            MoveUp();
+        }
         
+        public void HideTextBox()
+        {
+            MoveDown();
+            
+            // clear text when animation has finished
+            Invoke(nameof(ClearText), duration);
+        }
         
-        [ContextMenu("move up")]
-        public void MoveUp()
+
+        // TEXT
+        private void SetText(string text)
+        {
+            _textComponent.text = text;
+        }
+
+        private void ClearText()
+        {
+            _textComponent.text = "";
+        }
+
+
+        // MOVEMENT ANIMATION
+        private void MoveUp()
         {
             _doTweenAnimation.DOPlayForward();
         }
-        
-        [ContextMenu("move down")]
-        public void MoveDown()
+
+        private void MoveDown()
         {
             _doTweenAnimation.DOPlayBackwards();
         }
