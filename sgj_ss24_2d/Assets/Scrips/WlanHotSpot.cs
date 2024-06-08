31dfeb701e6ced9fa487eb01d518adf804b25bcf
Sebastian <sebastian.geheeb@gmx.de>
2024-06-08 10:14:10 +0200

    added wlan hotspot

--- sgj_ss24_2d/Assets/Scrips/WlanHotSpot.cs
+++ sgj_ss24_2d/Assets/Scrips/WlanHotSpot.cs
@@ -1,26 +1,17 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-public class Rain : MonoBehaviour
+public class WlanHotSpot : MonoBehaviour
 {
-    [SerializeField] private GameObject rainPrefab;
-    public float maxDropCount;
+    [SerializeField] private GameObject hotspotPrefab;
     public int spawnChance;
     public float height;
     public float width;
 
-    private List<GameObject> _rainDrops;
     private float _halfHeight;
     private float _halfWidth;
-
-    private void Start()
-    {
-        _rainDrops = new List<GameObject>();
-    }
-
+    
     private void Update()
     {
         Debug.DrawLine(new Vector3(-width, height, 0), new Vector3(width, height, 0));
@@ -32,9 +23,8 @@ public class Rain : MonoBehaviour
         {
             float randomX = Random.Range(-width, width);
             float randomY = Random.Range(-height, height);
-            var instance = Instantiate(rainPrefab);
-            instance.transform.position = new Vector3(randomX, randomY, 0);
-            _rainDrops.Add(instance);
+            Instantiate(hotspotPrefab);
+            hotspotPrefab.transform.position = new Vector3(randomX, randomY, 0);
         }
     }
 }
