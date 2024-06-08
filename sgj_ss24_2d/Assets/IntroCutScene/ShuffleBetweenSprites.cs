775629a67ee92fe7334f8075a81f9ea083042caf
gringames <grin.games@proton.me>
2024-06-08 16:08:12 +0200

    add ShuffleBetweenSprites for rain in initial cut scene

---
+++ sgj_ss24_2d/Assets/IntroCutScene/ShuffleBetweenSprites.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace IntroCutScene
+{
+    public class ShuffleBetweenSprites : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] spriteObjects;
+        [SerializeField] private int indexOfFirstActiveSprite = 0;
+        [SerializeField] private float timeBeforeShuffleStarts;
+        [SerializeField] private float timeBetweenShuffles;
+
+        private int _currentIndex;
+        private int _numberOfSprites;
+
+        private void Start()
+        {
+            _numberOfSprites = spriteObjects.Length;
+            _currentIndex = 0;
+            if (_numberOfSprites < 2) return;
+
+            DisableAllButFirst();
+            
+            InvokeRepeating(nameof(ShuffleSprites), timeBeforeShuffleStarts, timeBetweenShuffles);
+        }
+
+        private void DisableAllButFirst()
+        {
+            for (int i = 1; i < _numberOfSprites; i++)
+            {
+                spriteObjects[i].SetActive(false);
+            }
+        }
+
+        private void ShuffleSprites()
+        {
+            // disable old
+            spriteObjects[_currentIndex].SetActive(false);
+            
+            // enable new
+            _currentIndex = (_currentIndex + 1) % _numberOfSprites;
+            spriteObjects[_currentIndex].SetActive(true);
+        }
+    }
+}
