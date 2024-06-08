1059c727e12217981207bbbb40a4850c5aafa5e1
Sebastian <sebastian.geheeb@gmx.de>
2024-06-08 18:45:50 +0200

    added mock animations

---
+++ sgj_ss24_2d/Assets/Scrips/AnimationTEst.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTEst : MonoBehaviour
+{
+    public float speed;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+      
+        transform.position += new Vector3(horizontal, vertical, 0).normalized * speed * Time.deltaTime;
+
+        ResetAll();
+        
+        if(horizontal < 0)
+        {
+            _animator.SetBool("L", true);
+        }
+        else if (horizontal > 0)
+        {
+            _animator.SetBool("R", true);
+        }
+
+        if (vertical < 0)
+        {
+            _animator.SetBool("U", true);
+        }
+        else if (vertical > 0)
+        {
+            _animator.SetBool("O", true);
+        }
+    }
+
+
+    private void ResetAll()
+    {
+        _animator.SetBool("L", false);
+        _animator.SetBool("R", false);
+        _animator.SetBool("O", false);
+        _animator.SetBool("U", false);
+    }
+}
