1fc5d573ca951514c662b25be1a7d9d4c2efee34
Michl <michlbayer1@googlemail.com>
2024-06-08 09:08:40 +0200

    Caracter controller Prototype da und PlayerClass angefangen

--- sgj_ss24_2d/Assets/Scrips/PlayerMovement.cs
+++ sgj_ss24_2d/Assets/Scrips/PlayerMovement.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     public float startScale = 2;
@@ -12,64 +14,74 @@ public class PlayerMovement : MonoBehaviour
     public float minSpeed;
     public float shrinkFactor;
 
-    private bool leftInput;
-    private bool rightInput;
-    private bool upInput;
-    private bool downInput;
+    private float _upInput;
+    private float _downInput;
+    private float _rightInput;
+    private float _leftInput;
 
-    private KeyCode keyForLeft = KeyCode.A;
-    private KeyCode keyForRight = KeyCode.D;
-    private KeyCode keyForUp = KeyCode.UpArrow;
-    private KeyCode keyForDown = KeyCode.DownArrow;
-    
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody2D;
+
+    public InputActionReference PlayerOne;
+    public InputActionReference PlayerTwo;
+
+    private void Start()
     {
-        Invoke(nameof(ChangeInputRandomly),6f);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-        if (transform.localScale.x < minScale)
-        {
-            Debug.Log("GAME OVER!!");
-            return;
-        }
-        
-        leftInput = Input.GetKey(keyForLeft);
-        rightInput = Input.GetKey(keyForRight);
-        upInput = Input.GetKey(keyForUp);
-        downInput = Input.GetKey(keyForDown);
+        TestInput();
 
-        transform.localScale -= new Vector3(shrinkFactor * Time.deltaTime, shrinkFactor * Time.deltaTime, shrinkFactor * Time.deltaTime);
+        ShrinkBlob();
 
+        if (IsDeath())
+            Debug.Log("IsDeath Now");
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveVec = Vector2.zero;
-        
-        if (leftInput)
-        {
-            moveVec -= new Vector2(1, 0);
-        }       
-        if (rightInput)
-        {
-            moveVec += new Vector2(1, 0);
-        }       
-        if (upInput)
-        {
-            moveVec += new Vector2(0, 1);
-        }       
-        if (downInput)
-        {
-            moveVec -= new Vector2(0, 1);
-        }
+        Move();
+    }
 
-        float curSpeed = Mathf.Lerp(minSpeed,maxSpeed,Mathf.InverseLerp(startScale, minScale, transform.localScale.x));
-        transform.position += new Vector3(moveVec.x, moveVec.y, 0).normalized * curSpeed;
+    private void TestInput()
+    {
+        var v = PlayerOne.action.ReadValue<Vector2>();
+        if (v.x > 0)
+            SetRight(v.x);
+        if (v.y > 0)
+            SetUp(v.y);
+        if (v.x < 0)
+            SetLeft(v.x);
+        if (v.y < 0)
+            SetDown(v.y);
+
+        Debug.Log("P1:" + v);
+        
+        v = PlayerTwo.action.ReadValue<Vector2>();
+        if (v.x > 0)
+            SetRight(v.x);
+        if (v.y > 0)
+            SetUp(v.y);
+        if (v.x < 0)
+            SetLeft(v.x);
+        if (v.y < 0)
+            SetDown(v.y);
+        
+        
+        Debug.Log("P2:" + v);
+    }
+
+    private void Move()
+    {
+        Vector2 moveDir = new Vector2(_rightInput - _leftInput, _upInput - _downInput);
+        moveDir.Normalize();
+
+        float currSpeed = Mathf.Lerp(minSpeed, maxSpeed,
+            Mathf.InverseLerp(startScale, minScale, transform.localScale.x));
+
+        Vector2 velocity = new Vector2(moveDir.x, moveDir.y) * currSpeed;
+        _rigidbody2D.AddForce(velocity, ForceMode2D.Force);
     }
 
     public void DropCollected(float value)
@@ -77,11 +89,37 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale += new Vector3(value, value, value);
     }
 
-    private void ChangeInputRandomly()
+    private void ShrinkBlob()
     {
-        keyForLeft = KeyCode.LeftArrow;
-        keyForRight = KeyCode.RightArrow;
-        keyForUp = KeyCode.W;
-        keyForDown = KeyCode.S;
+        float subtrator = shrinkFactor * Time.deltaTime;
+        Vector3 currScale = transform.localScale;
+        Vector3 newScale = new Vector3(currScale.x - subtrator, currScale.y - subtrator,
+            currScale.z - subtrator);
+        transform.localScale = newScale;
+    }
+
+    private bool IsDeath()
+    {
+        return transform.localScale.x <= minScale;
+    }
+
+    public void SetUp(float f)
+    {
+        _upInput = Mathf.Clamp01(Mathf.Abs(f));
+    }
+
+    public void SetDown(float f)
+    {
+        _downInput = Mathf.Clamp01(Mathf.Abs(f));
+    }
+
+    public void SetRight(float f)
+    {
+        _rightInput = Mathf.Clamp01(Mathf.Abs(f));
+    }
+
+    public void SetLeft(float f)
+    {
+        _leftInput = Mathf.Clamp01(Mathf.Abs(f));
     }
 }
