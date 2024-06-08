1eac80763dd67b6803ee9463adb0c0eccf03b2f9
gringames <grin.games@proton.me>
2024-06-08 17:28:23 +0200

    move standin images in separate folder + add thunder

---
+++ sgj_ss24_2d/Assets/IntroCutScene/Flash.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace IntroCutScene
+{
+    public class Flash : MonoBehaviour
+    {
+        [SerializeField] private float timeFlashing;
+
+        private SpriteRenderer _spriteRenderer;
+        
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.gameObject.SetActive(false);
+        }
+
+        public void StartFlash()
+        {
+            _spriteRenderer.gameObject.SetActive(true);
+            Invoke(nameof(StopFlash), timeFlashing);
+        }
+        
+        public void StopFlash()
+        {
+            _spriteRenderer.gameObject.SetActive(false);
+        }
+    }
+}
