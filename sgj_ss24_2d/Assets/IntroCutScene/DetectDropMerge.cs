ec86d7bd3eadf1be7f56156faf1a5d56e395edae
Julian Geheeb <geheeb@in.tum.de>
2024-06-09 10:13:00 +0200

    Fixes and fixes

--- sgj_ss24_2d/Assets/IntroCutScene/DetectDropMerge.cs
+++ sgj_ss24_2d/Assets/IntroCutScene/DetectDropMerge.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace IntroCutScene
@@ -6,11 +7,21 @@ namespace IntroCutScene
     [RequireComponent(typeof(Collider2D))]
     public class DetectDropMerge : MonoBehaviour
     {
-        [SerializeField] private UnityEvent onDropsCollided;
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("Drop")) return;
-            
+
+            GameObject.Find("blitzii").GetComponent<Animator>().enabled = true;
+            StartCoroutine(LoadScene());
+        }
+
+        private IEnumerator LoadScene()
+        {
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+
             FindObjectOfType<SceneLoader>().LoadNextScene();
         }
     }
