31e4f7d437ad37521be9327cea2fe7c72066f9f4
Michl <michlbayer1@googlemail.com>
2024-06-09 07:48:36 +0200

    Vibrator go BRRRRRR

--- sgj_ss24_2d/Assets/Scrips/PlayerClass.cs
+++ sgj_ss24_2d/Assets/Scrips/PlayerClass.cs
@@ -5,6 +5,7 @@ using IntroCutScene;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.VirtualTexturing;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
@@ -15,33 +16,32 @@ public class PlayerClass : MonoBehaviour
     [SerializeField] private Card _card;
     [SerializeField] private bool isTutorial;
     private bool started;
-    
+
     private void Awake()
     {
         blobController = FindObjectOfType<BlobController>();
-        
+
         if (isTutorial)
         {
-            
             //BlobController[] blobControllers = FindObjectsOfType<BlobController>();
             blobController = Instantiate(blobPrefab).GetComponent<BlobController>();
-            
-            blobController.StartBlob(); 
+
+            blobController.StartBlob();
             blobController.StopShrinking();
             if (FindObjectOfType<PlayerInputManager>().playerCount == 1)
             {
                 blobController.AddComponent<DetectDropMerge>();
                 blobController.GetComponents<CircleCollider2D>()[1].enabled = true;
-                
+
                 _card = Instantiate(_card);
                 _card.cardActions = new List<CardAction>() {CardAction.Up, CardAction.Down};
                 //blobController = blobControllers[0];
 
                 SceneManager.sceneLoaded += Subscriber;
-                
+
                 DontDestroyOnLoad(gameObject);
             }
-            
+
             if (FindObjectOfType<PlayerInputManager>().playerCount == 2)
             {
                 _card = Instantiate(_card);
@@ -75,55 +75,77 @@ public class PlayerClass : MonoBehaviour
     public void StartGame(InputAction.CallbackContext obj)
     {
         if (started) return;
-        
-        started = true;    
-        if(!isTutorial)
+
+        started = true;
+        if (!isTutorial)
             FindObjectOfType<GameLoop>().StartGameLoop();
         else
         {
-            blobController.StartBlob(); 
+            blobController.StartBlob();
             blobController.StopShrinking();
         }
     }
 
     public void ActionOnperformedMove(InputAction.CallbackContext obj)
     {
-       Debug.Log("move");
-       Vector2 input = obj.ReadValue<Vector2>();
+        Debug.Log("move");
+        Vector2 input = obj.ReadValue<Vector2>();
 
-       float schranke = 0.1f;
+        float schranke = 0.1f;
 
-       foreach (var cardAction in _card.cardActions)
-       {
-           switch (cardAction)
-           {
-               case CardAction.Up:
-                   if(input.y >= 0)
-                       blobController.SetUp(input.y > schranke ? input.y : 0);
-                   break;
-               case CardAction.Down:
-                   if(input.y < 0)
-                       blobController.SetDown(input.y < -schranke ? input.y : 0);
-                   break;
-               case CardAction.Right:
-                   if(input.x >= 0)
-                       blobController.SetRight(input.x > schranke ? input.x : 0);
-                   break;
-               case CardAction.Left:
-                   if(input.x < 0)
-                       blobController.SetLeft(input.x < -schranke ? input.x : 0);
-                   break;
-           }
-       }
+        foreach (var cardAction in _card.cardActions)
+        {
+            switch (cardAction)
+            {
+                case CardAction.Up:
+                    if (input.y >= 0)
+                    {
+                        blobController.SetUp(input.y > schranke ? input.y : 0);
+                        if (input.y > schranke)
+                            VibratorGoBrrrrr(input.y);
+                        else
+                            VibrationGOAwai();
+                    }
+                    else
+                        VibrationGOAwai();
+
+                    break;
+                case CardAction.Down:
+                    if (input.y < 0)
+                        blobController.SetDown(input.y < -schranke ? input.y : 0);
+                    break;
+                case CardAction.Right:
+                    if (input.x >= 0)
+                        blobController.SetRight(input.x > schranke ? input.x : 0);
+                    break;
+                case CardAction.Left:
+                    if (input.x < 0)
+                        blobController.SetLeft(input.x < -schranke ? input.x : 0);
+                    break;
+            }
+        }
     }
-    
+
     public void ActionOnperformedDash(InputAction.CallbackContext obj)
     {
-      blobController.Dash();
+        blobController.Dash();
     }
-    
+
     public void SetCard(Card newCard)
     {
         _card = newCard;
+    }
+
+    public float lowFdiv;
+    public float hightFdiv;
+    public void VibratorGoBrrrrr(float strangth)
+    {
+        strangth = Mathf.Abs(strangth);
+        GetComponent<PlayerInput>().GetDevice<Gamepad>().SetMotorSpeeds(strangth / lowFdiv, strangth / hightFdiv);
+    }
+
+    private void VibrationGOAwai()
+    {
+        GetComponent<PlayerInput>().GetDevice<Gamepad>().SetMotorSpeeds(0, 0);
     }
 }
